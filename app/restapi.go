cd12c9e5495558e6ffb64d4a4ae4459f0fc19372
Vincent Rasquier <vrasquier@keley-live.com>
2016-11-15 17:28:11 +0100

    Add first json decoder

--- app/restapi.go
+++ app/restapi.go
@@ -1,10 +1,24 @@
 package app
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
+	"github.com/eatbytes/razboy/core"
 	"github.com/urfave/cli"
 )
+
+type apirequest struct {
+	cmd    string
+	scope  string
+	method int
+}
+
+type apidata struct {
+	config  core.Config
+	request apirequest
+}
 
 func (app *AppInterface) Api(c *cli.Context) {
 	var port string
@@ -17,7 +31,22 @@ func (app *AppInterface) Api(c *cli.Context) {
 }
 
 func (app *AppInterface) apishell(w http.ResponseWriter, req *http.Request) {
+	var (
+		decoder *json.Decoder
+		data    apidata
+		err     error
+	)
 
+	decoder = json.NewDecoder(req.Body)
+	err = decoder.Decode(&data)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer req.Body.Close()
+
+	log.Println(data.request.cmd)
 }
 
 func (app *AppInterface) apiphp(w http.ResponseWriter, req *http.Request) {
