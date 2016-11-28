b1c472ea1e6671ad09e273360d2583781c177c84
root <root@localhost.localdomain>
2016-11-28 19:38:14 +0100

    Docker script

--- res/docker/start-web.sh
+++ res/docker/start-web.sh
@@ -1,4 +1,4 @@
 cd ..
 
-docker run -d -v res/backdoor:/var/www/html --name php-running-razboynik php-razboynik
-docker rm -f /php-running-razboynik
+docker run -d -v /res/backdoor:/var/www/html --name php-running-razboynik php-razboynik
+#docker rm -f /php-running-razboynik
