a4bc503f80c870d1d889919b0ec671119901b184
root <root@localhost.localdomain>
2016-11-29 19:21:32 +0100

    New flags and docs + rename file

--- cmd/root.go
+++ cmd/root.go
@@ -23,19 +23,22 @@ import (
 	"github.com/spf13/viper"
 )
 
-var method string
-var parameter string
-var key string
-var shellmethod string
-var proxy string
-var encoding string
-var raw bool
-var debug bool
-var silent bool
+var (
+	method      string
+	parameter   string
+	key         string
+	shellmethod string
+	encoding    string
+	proxy       string
+	shellscope  string
+	raw         bool /* @DEPRECATED */
+	debug       bool
+	silent      bool
+)
 
 var RootCmd = &cobra.Command{
 	Use:   "razboynik",
-	Short: "A brief description of your application",
+	Short: "Virtual shell on webserver thanks to file upload vulnerability",
 	Long:  ``,
 }
 
@@ -52,13 +55,14 @@ func init() {
 
 	RootCmd.PersistentFlags().StringVarP(&method, "method", "m", "GET", "Method to use. Ex: -m POST")
 	RootCmd.PersistentFlags().StringVarP(&parameter, "parameter", "p", "razboynik", "Parameter to use. Ex: -p test")
-	RootCmd.PersistentFlags().StringVarP(&key, "key", "k", "FromRussiaWithLove<3", "Key to unlock optional small protection. Ex: -k keytounlock")
-	RootCmd.PersistentFlags().StringVarP(&shellmethod, "shellmethod", "s", "system", "")
-	RootCmd.PersistentFlags().StringVar(&proxy, "proxy", "", "Proxy url where request will be sent before. Ex: -p http://localhost:8080")
-	RootCmd.PersistentFlags().StringVarP(&encoding, "encoding", "e", "base64", "")
-	RootCmd.PersistentFlags().BoolVarP(&raw, "raw", "r", false, "raw")
-	RootCmd.PersistentFlags().BoolVarP(&debug, "debug", "d", false, "Print more information")
-	RootCmd.PersistentFlags().BoolVar(&silent, "silent", false, "Don't print anything else than result or error")
+	RootCmd.PersistentFlags().StringVarP(&key, "key", "k", "", "Key to unlock optional small protection. Ex: -k keytounlock")
+	RootCmd.PersistentFlags().StringVarP(&shellmethod, "shellmethod", "s", "system", "System function used in php script. Ex: -s shell_exec")
+	RootCmd.PersistentFlags().StringVarP(&encoding, "encoding", "e", "base64", "Encoding of the request. Ex: -e base64")
+	RootCmd.PersistentFlags().StringVar(&shellscope, "scope", "", "Scope inside the shell. Ex: --scope /var")
+	RootCmd.PersistentFlags().StringVar(&proxy, "proxy", "", "Proxy url where request will be sent before. Ex: --proxy http://localhost:8080")
+	RootCmd.PersistentFlags().BoolVarP(&raw, "raw", "r", false, "*DEPRECATED* (use encoding instead) raw")
+	RootCmd.PersistentFlags().BoolVar(&debug, "debug", false, "Print more information for debugging. Ex: --debug")
+	RootCmd.PersistentFlags().BoolVar(&silent, "silent", false, "Don't print anything else than result or error. Ex: --silent")
 }
 
 func initConfig() {
